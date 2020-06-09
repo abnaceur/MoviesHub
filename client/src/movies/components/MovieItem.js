01abc43b79e8f1795675f91f707d9bd60500c46e
melchior <melchiorbengtsson@gmail.com>
2020-06-09 21:01:47 +0200

    ok'

--- client/src/movies/components/MovieItem.js
+++ client/src/movies/components/MovieItem.js
@@ -5,12 +5,18 @@ import "../../App.css";
 const MovieItem = (props) => {
   return (
     <div className="movie_item">
+      <div className="movie_info">337</div>
       <img
         src={props.movie.coverImage}
         alt={props.movie.name}
         className="movie_img"
       />
+
       <p className="movie_title">{props.movie.name}</p>
+      <p className="movie_rating">
+        {props.movie.rating}/5
+        <div className="movie_year">{props.movie.year}</div>
+      </p>
     </div>
   );
 };
