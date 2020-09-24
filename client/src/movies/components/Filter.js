62ceac2fb63809ba926a6939bbd350c1cb63ae22
Sakala85 <42222200+Sakala85@users.noreply.github.com>
2020-09-24 10:01:38 +0200

    ok

--- client/src/movies/components/Filter.js
+++ client/src/movies/components/Filter.js
@@ -1,24 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactStars from "react-rating-stars-component";
+import ReactScrollWheelHandler from "react-scroll-wheel-handler";
 import MovieList from "./MovieList";
-
+import { useHttpClient } from "../../shared/hooks/http-hook";
 import "./Filter.css";
 import "../../App.css";
 
 const Filter = (props) => {
+  const [moviesLoaded, setMoviesLoaded] = useState(null);
   const [showCategory, setShowCategory] = useState(false);
+  const [page, setPage] = useState(1);
   const [rating, setRating] = useState(1);
   const [movies, setMovies] = useState(props.movies);
-  const [Category, setCategory] = useState("All");
+  const [Category, setCategory] = useState({ id: 0, value: "All" });
+  const [CategoryList, setCategoryList] = useState(null);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const fetchMovies = async (rating, Category) => {
+    try {
+      const responseData = await sendRequest(
+        `https://api.themoviedb.org/3/discover/movie?api_key=03629fbb07a3c96f17fbde76c54e3812&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+      );
+      if (moviesLoaded) {
+        let tmpMovies = moviesLoaded.results;
+        tmpMovies.push(responseData.results);
+        console.log(tmpMovies);
+      }
+      setMoviesLoaded(responseData);
+      let tmp = page + 1;
+      setPage(tmp);
+    } catch (err) { }
+  };
+  useEffect(() => {
+    const fetchCategory = async () => {
+      try {
+        const responseData = await sendRequest(
+          `https://api.themoviedb.org/3/genre/movie/list?api_key=03629fbb07a3c96f17fbde76c54e3812&language=en-US
+          `
+        );
+        setCategoryList(responseData.genres);
+      } catch (err) { }
+    };
+    fetchCategory();
+  }, [sendRequest]);
   const ratingChanged = (newRating) => {
     setRating(newRating);
-    // const filteredUsers1 = props.items.filter(
-    //   (user) => user.age >= formState.inputs.ageMin.value
-    // );
-    if (Category === "All") {
+    if (Category.value === "All") {
       setMovies(props.movies.filter((movie) => movie.rating >= newRating));
     } else {
-      // let TmpMovies = props.movies.filter((movie) => movie.category === Category);
       setMovies(props.movies.filter((movie) => movie.rating >= newRating));
     }
     console.log(rating);
@@ -27,17 +55,17 @@ const Filter = (props) => {
     if (!showCategory) {
       setShowCategory(true);
     } else {
-      console.log(event.target.id);
-      setCategory(event.target.id);
+      setCategory({ id: event.target.id, value: event.target.value });
       if (rating === 1) {
-        // setMovies(props.movies.filter((movie) => movie.category === event.target.id));
       } else {
-        // let TmpMovies = props.movies.filter((movie) => movie.rating >== rating);
-        // setMovies(props.movies.filter((movie) => movie.category === event.target.id));
       }
       setShowCategory(false);
     }
   };
+
+  const setAll = () => {
+    setCategory({ id: 0, value: "All" });
+  }
 
   return (
     <React.Fragment>
@@ -54,57 +82,40 @@ const Filter = (props) => {
         <div className="filterElement">
           <button
             onClick={developCategory}
-            id={Category}
+            id={Category.id}
             className="filterItem"
           >
-            {Category}
+            {Category.value}
           </button>
-          {showCategory && (
-            <div className="">
-              <button
+          {showCategory && CategoryList &&
+            CategoryList.map((Category) => {
+              return (<button
                 className="filterItem"
-                id="Action"
+                id={Category.id}
+                value={Category.name}
                 onClick={developCategory}
               >
-                {" "}
-                Action{" "}
-              </button>
-              <button
-                className="filterItem"
-                id="Adventure"
-                onClick={developCategory}
-              >
-                {" "}
-                Adventure{" "}
-              </button>
-              <button
-                className="filterItem"
-                id="Horreur"
-                onClick={developCategory}
-              >
-                {" "}
-                Horreur{" "}
-              </button>
-              <button
-                className="filterItem"
-                id="Drama"
-                onClick={developCategory}
-              >
-                {" "}
-                Drama{" "}
-              </button>
-              <button className="filterItem" id="All" onClick={developCategory}>
-                {" "}
-                All{" "}
-              </button>
-            </div>
-          )}
+                {Category.name}
+              </button>)
+            })
+          }
+          <button
+            className="filterItem"
+            id="0"
+            onClick={setAll}
+            value="All"
+          >
+            All
+          </button>
         </div>
       </div>
       <div className="movies_container">
-        <MovieList movies={movies} />
+        <button onClick={fetchMovies}>TMP</button>
+        {moviesLoaded && <ReactScrollWheelHandler downHandler={fetchMovies}>
+          <MovieList movies={moviesLoaded} />
+        </ReactScrollWheelHandler>}
       </div>
-    </React.Fragment>
+    </React.Fragment >
   );
 };
 
--- client/src/movies/components/MovieList.js
+++ client/src/movies/components/MovieList.js
@@ -1,46 +1,14 @@
 import React, { useState, useEffect } from "react";
 import MovieItem from "./MovieItem";
-import ReactScrollWheelHandler from "react-scroll-wheel-handler";
+import Filter from "./Filter";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 import "./Filter.css";
 import "../../App.css";
 
 const MovieList = (props) => {
-  const [moviesLoaded, setMoviesLoaded] = useState(null);
-  const [page, setPage] = useState(1);
-  const { isLoading, error, sendRequest, clearError } = useHttpClient();
-  const loadMoreMovies = () => {
-    setMoviesLoaded(moviesLoaded + 7);
-  };
-  const fetchMovies = async () => {
-    try {
-      const responseData = await sendRequest(
-        `https://api.themoviedb.org/3/discover/movie?api_key=03629fbb07a3c96f17fbde76c54e3812&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
-      );
-      if (moviesLoaded) {
-        let tmpMovies = moviesLoaded.results;
-        tmpMovies.push(responseData.results);
-        console.log(tmpMovies);
-      }
-      setMoviesLoaded(responseData);
-      let tmp = page + 1;
-      setPage(tmp);
-    } catch (err) {}
-  };
-  // useEffect(() => {
-  //   fetchMovies();
-  //   console.log("MoviesFetched");
-  // }, [fetchMovies]);
   return (
     <React.Fragment>
-      <button onClick={fetchMovies}>OKOK</button>
-      <ReactScrollWheelHandler downHandler={fetchMovies}>
-        {moviesLoaded &&
-          moviesLoaded !== null &&
-          moviesLoaded.results.map((movie, i) => {
-            return <MovieItem key={movie.id} movie={movie} />;
-          })}
-      </ReactScrollWheelHandler>
+      {props.movies.id}
     </React.Fragment>
   );
 };
