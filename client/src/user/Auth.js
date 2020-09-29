7bf271e7f2b3330d09375f2021d676673c150bef
akhoucha <35508550+akhoucha@users.noreply.github.com>
2020-09-29 18:20:11 +0200

    front sign up

--- client/src/user/Auth.js
+++ client/src/user/Auth.js
@@ -1,12 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, Button } from "react";
 import LogoFacebook from "../logo/Logo-Facebook-rond.png";
 import Logo42 from "../logo/logo_42.jpg";
 import LeftArrow from "../logo/left-arrow.png";
 import RightArrow from "../logo/right-arrow.png";
 import "./Auth.css";
 import "../App.css";
+import { useHttpClient } from "../shared/hooks/http-hook";
+import ErrorModal from "../shared/UIElements/ErrorModal";
+import LoadingSpinner from "../shared/UIElements/LoadingSpinner";
+import { Comment, Header } from "semantic-ui-react";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../shared/util/validators";
+import { useForm } from "../shared/hooks/form-hook";
+// import { Input } from "@material-ui/core";
+import Input from "../shared/FormElements/Input";
 
 const Auth = () => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [formState, inputHandler] = useForm(
+    {
+      username: {
+        value: "",
+        isValid: false,
+      },
+      firstname: {
+        value: "",
+        isValid: false,
+      },
+      lastname: {
+        value: "",
+        isValid: false,
+      },
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
   const [loginMode, setLoginMode] = useState("left-panel-active");
   const switchModeHandler = () => {
     if (loginMode === "right-panel-active") {
@@ -15,14 +52,38 @@ const Auth = () => {
       setLoginMode("right-panel-active");
     }
   };
+  const fetchUser = async () => {
+    console.log("ok");
+
+    if (loginMode) {
+      try {
+        await sendRequest(
+          `http://localhost:5000/api/user/login`,
+          "POST",
+
+          JSON.stringify({
+            username: formState.inputs.username.value,
+            firstname: formState.inputs.firstname.value,
+            lastname: formState.inputs.lastname.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+          {
+            "Content-Type": "application/json",
+          }
+        );
+      } catch (err) {}
+    } else {
+    }
+  };
 
   return (
     <React.Fragment>
       <div className="auth_page">
         <div className={"container " + loginMode} id="container">
           <div className="form-container sign-up-container">
-            <form action="#">
-              <h1>Create Account</h1>
+            <form onSubmit={fetchUser}>
+              <h1>Create User</h1>
               <div className="social-container">
                 <a href="./" className="social">
                   <img
@@ -39,25 +100,71 @@ const Auth = () => {
                   />
                 </a>
               </div>
-              <input type="text" placeholder="Username" />
-              <input
-                className="input_same_line"
+              <Input
+                id="Username"
+                placeholder="Username"
+                element="input"
                 type="text"
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(2)]}
+                label="Username"
+                errorText="Please enter a valid Username. (2 characters min.)"
+                initialValue=""
+                initialValid={false}
+                onInput={inputHandler}
+              />
+              <Input
+                id="Firstname"
                 placeholder="Firstname"
-              />
-              <input
-                className="input_same_line"
+                element="input"
                 type="text"
-                placeholder="Lastname"
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(2)]}
+                label="firstname"
+                errorText="Please enter a valid firstname. (2 characters min.)"
+                initialValue=""
+                initialValid={false}
+                onInput={inputHandler}
               />
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <input type="password" placeholder="Retape Password" />
-              <button>Sign Up</button>
+              <Input
+                id="Lastname"
+                placeholder="Lastname"
+                element="input"
+                type="text"
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(2)]}
+                label="lastname"
+                errorText="Please enter a valid lastname. (2 characters min.)"
+                initialValue=""
+                initialValid={false}
+                onInput={inputHandler}
+              />
+              <Input
+                id="Email"
+                placeholder="Email"
+                element="input"
+                type="email"
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(2)]}
+                label="email"
+                errorText="Please enter a valid email. (2 characters min.)"
+                initialValue=""
+                initialValid={false}
+                onInput={inputHandler}
+              />
+              <Input
+                id="Password"
+                placeholder="Password"
+                element="input"
+                type="text"
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(2)]}
+                label="password"
+                errorText="Please enter a valid password. (2 characters min.)"
+                initialValue=""
+                initialValid={false}
+                onInput={inputHandler}
+              />
+              <button onClick={fetchUser}>Sign Up</button>
             </form>
           </div>
           <div className="form-container sign-in-container">
-            <form action="#">
+            <form onSubmit={fetchUser}>
               <h1>Sign in</h1>
               <div className="social-container">
                 <a href="./" className="social">
