cabe75b65d741a207d04901baaa3f40f620f1e02
Yixuan WANG <yiwang@student.42.fr>
2020-09-30 12:08:52 +0200

    SERVER : - Setup mongoDB. -Create folders structure.

--- server/app.js
+++ server/app.js
@@ -1,68 +1,90 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const http = require("http");
-var unirest = require("unirest");
+require('dotenv').config();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var db = require('./config/dbConnection');
+var bodyParser = require('body-parser');
+const helmet = require('helmet');
+let mongoose = require('mongoose');
 
-const router = require("./routes/router");
-const userRoutes = require("./routes/user-routes");
-const commentRoutes = require("./routes/comment-routes");
+// Main app
+var app = express();
 
-const app = express();
-const server = http.createServer(app);
+//Use hamlet 
+app.use(helmet())
 
-app.use((req, res, next) => {
-  next();
-});
+// Logs
+app.use(logger('dev'));
+
+// important if behind a proxy to ensure client IP is passed to req.ip
+//app.enable('trust proxy'); 
+
+
+//Map global promise
+mongoose.Promise = global.Promise;
+
+
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Fix CORS errors
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, PATCH, DELETE');
+    return res.status(200).json({});
+  };
   next();
 });
-app.use(router);
-app.use("/api/user", userRoutes);
-app.use("/api/comments", commentRoutes);
 
-app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
-  throw error;
-});
-app.use((error, req, res, next) => {
-  if (res.headerSent) {
-    return next(error);
-  }
-  res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occured !" });
+// Routes
+var indexRouter = require('./api/routes/index');
+
+// Open connection to the database
+db.once('open', function () {
+  console.log('Connected to mongodb!');
 });
 
-var req = unirest(
-  "GET",
-  "https://movie-database-imdb-alternative.p.rapidapi.com/"
-);
-
-req.query({
-  page: "1",
-  r: "json",
-  s: "Avengers Endgame",
+// Catch errors on database connection failure
+db.on('error', function (err) {
+  console.log('Error while connecting to database: ', err)
 });
 
-req.headers({
-  "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
-  "x-rapidapi-key": "b4a2e3aec7mshe08952e9aba8a5fp1e68b8jsn1ea8797064ee",
-  useQueryString: true,
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Call routes API
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-req.end(function (res) {
-  // if (res.error) throw new Error(res.error);
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  console.log(res.body);
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
-server.listen(process.env.PORT || 5000, () =>
-  console.log(`Server has started.`)
-);
+
+module.exports = app;
